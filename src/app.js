72ac0b774a3d27f3b828b7c7e45a9cb59abc541a
ThiagoCCR <thiago.ccr96@gmail.com>
2022-09-01 14:31:07 -0300

    feat getParticipants

--- src/app.js
+++ src/app.js
@@ -16,17 +16,57 @@ mongoClient.connect().then(() => {
   db = mongoClient.db("buzzquizz");
 });
 
-//POST /participants
+app.post("/participants", async (req, res) => {
+  const { name } = req.body;
+  let listOfParticipants;
 
-//GET /participants
+  //testar se o nome e valido
+  if (!name) {
+    return res.status(422).send("Nome de usuário inválido!");
+  }
+
+  //testar se já existe usuário com o mesmo nome
+  try {
+    listOfParticipants = await db.collection("participants").find().toArray();
+  } catch (error) {
+    return res.sendStatus(500);
+  }
+
+  if (listOfParticipants.includes(name)) {
+    return res.status(409).send("Usuário já cadastrado!");
+  }
+
+  //adc na db o usuário
+  try {
+    const addedParticipant = await db
+      .collection("participants")
+      .insertOne({ name: name, lastStatus: Date.now() });
+    res.sendStatus(201);
+  } catch (error) {
+    return res.sendStatus(500);
+  }
+
+  //adc status de entrada
+  
+});
+
+app.get("/participants", async (req, res) => {
+  try {
+    const listOfParticipants = await db
+      .collection("participants")
+      .find()
+      .toArray();
+    res.status(200).send(listOfParticipants);
+  } catch (error) {
+    return res.sendStatus(500);
+  }
+});
 
 //POST /messages
 
 //GET /messages
 
 //POST /stayus
-
-
 
 app.listen(5000, () => {
   console.log("Listening on Port 5000");
