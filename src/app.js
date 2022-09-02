dc47c8b1e2f6d6694a38017988ee28acf2222b7e
ThiagoCCR <thiago.ccr96@gmail.com>
2022-09-02 16:24:35 -0300

    feat postStatus

--- src/app.js
+++ src/app.js
@@ -102,7 +102,7 @@ app.get("/messages", async (req, res) => {
     try {
       const listOfMessages = await db.collection("messages").find().toArray();
       const filteredMessages = listOfMessages.reverse().filter((val, index) => {
-        if (val.to === user || val.to === "Todos") {
+        if (val.to === user || val.to === "Todos" || val.from === user) {
           return val;
         }
       });
@@ -128,6 +128,12 @@ app.post("/messages", async (req, res) => {
     abortEarly: false,
   });
 
+  if (validation.error) {
+    return res
+      .status(422)
+      .send(validation.error.details.map((res) => res.message));
+  }
+
   try {
     const listOfParticipants = await db
       .collection("participants")
@@ -136,21 +142,49 @@ app.post("/messages", async (req, res) => {
 
     if (
       listOfParticipants.filter(
-        (val) => val.name.toLowerCase() === name.toLowerCase()
+        (val) => val.name.toLowerCase() === User.toLowerCase()
       ).length === 0
     ) {
-      return res.status(409).send("Usuário já cadastrado!");
+      return res.status(409).send("Usuário inexistente!");
     }
+
+    const addedMessage = await db
+      .collection("messages")
+      .insertOne(templateMessage);
+
+    res.sendStatus(201);
   } catch (error) {
     return res.sendStatus(500);
   }
-
-  if (validation.error) {
-    return res.status(422).send(validation.error.details);
-  }
 });
 
-//POST /stayus (FAZER)
+app.post("/status", async (req, res) => {
+  const username = req.headers.user;
+
+  if (!username) {
+    return res.sendStatus(422);
+  }
+
+  try {
+    const listOfParticipants = await db
+      .collection("participants")
+      .find()
+      .toArray();
+
+    if (
+      listOfParticipants.filter(
+        (val) => val.name.toLowerCase() === username.toLowerCase()
+      ).length === 0
+    ) {
+      return res.sendStatus(404);
+    }
+
+    const addedParticipant = await db
+      .collection("participants")
+      .insertOne({ name: username, lastStatus: Date.now() });
+    res.sendStatus(200);
+  } catch (error) {}
+});
 
 app.listen(5000, () => {
   console.log("Listening on Port 5000");
